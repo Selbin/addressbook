7c8ded5b47e7d1ffdba2f8919add07d5696afc33
Selbin_C_R <selbinzr@gmail.com>
2020-09-05 17:47:42 +0530

    added route for getting address by id

--- server/routes/indexRoutes.js
+++ server/routes/indexRoutes.js
@@ -4,5 +4,6 @@ const addressHandler = require('../controllers/addressController')
 
 router.post('/add', addressHandler.addAddress)
 router.get('/get', addressHandler.getAddress)
+router.get('/get/:id', addressHandler.getAddressById)
 
 module.exports = router
