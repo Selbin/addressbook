8d5cd9be586fd9a607da8082698d47bb512d0f5b
Selbin_C_R <selbinzr@gmail.com>
2020-09-05 17:42:33 +0530

    added routes for getting address

--- server/routes/indexRoutes.js
+++ server/routes/indexRoutes.js
@@ -3,5 +3,6 @@ const router = express.Router()
 const addressHandler = require('../controllers/addressController')
 
 router.post('/add', addressHandler.addAddress)
+router.get('/get', addressHandler.getAddress)
 
 module.exports = router
