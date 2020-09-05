9a944e55b7076b58e2a8e5dea1f2879bed04fb20
Selbin_C_R <selbinzr@gmail.com>
2020-09-05 10:12:45 +0530

    implemeted add handler

---
+++ server/controllers/addressController.js
@@ -0,0 +1,17 @@
+const { exeQuery } = require('../database/database')
+const { successMsg, errorMsg, setResponseObj } = require('../helper/constant')
+
+const addAddress = async (req, res) => {
+  const { firstName, lastName, email, phoneNo, notes, dob } = req.body
+  const query = 'insert into users (first_name, last_name, email, phoneno, notes, dob) values ($1,$2, $3, $4, $5, $6) returning *'
+  try {
+    const result = await exeQuery(query, [firstName, lastName, email, phoneNo, notes, dob])
+    console.log(result)
+    res.status(200).json(setResponseObj(true, result.rows[0], successMsg, null))
+  } catch (error) {
+    console.log(error)
+    res.status(500).json(setResponseObj(false, null, errorMsg, errorMsg))
+  }
+}
+
+module.exports = { addAddress }
