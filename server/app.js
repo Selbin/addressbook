aedafa87544cb0d92c0f922f521b4437e04d6618
Selbin_C_R <selbinzr@gmail.com>
2020-09-05 10:11:57 +0530

    added base route

---
+++ server/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const dotEnv = require('dotenv')
+const indexRoutes = require('./routes/indexRoutes')
+const cors = require('cors')
+
+dotEnv.config()
+
+const app = express()
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/addressbook', indexRoutes)
+
+app.listen(process.env.APP_PORT, () => console.log('listening to: ', process.env.APP_PORT))
