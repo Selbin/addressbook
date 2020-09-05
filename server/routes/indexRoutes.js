dfa2151b5e921f13d6bd18d2257c185e89faaf02
Selbin_C_R <selbinzr@gmail.com>
2020-09-05 17:59:53 +0530

    added route for edit address

--- server/routes/indexRoutes.js
+++ server/routes/indexRoutes.js
@@ -5,5 +5,6 @@ const addressHandler = require('../controllers/addressController')
 router.post('/add', addressHandler.addAddress)
 router.get('/get', addressHandler.getAddress)
 router.get('/get/:id', addressHandler.getAddressById)
+router.put('/update/:id', addressHandler.editAddress)
 
 module.exports = router
