daa1f9cfcf371f48aa9ae08c6d48207ca19479fc
Selbin_C_R <selbinzr@gmail.com>
2020-09-06 09:32:39 +0530

    added validation middleware for add and edit apis

--- server/routes/indexRoutes.js
+++ server/routes/indexRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express')
 const router = express.Router()
 const addressHandler = require('../controllers/addressController')
+const { checkFields } = require('../middlewares/checkFields')
 
-router.post('/add', addressHandler.addAddress)
+router.post('/add', checkFields, addressHandler.addAddress)
 router.get('/get', addressHandler.getAddress)
 router.get('/get/:id', addressHandler.getAddressById)
-router.put('/update/:id', addressHandler.editAddress)
+router.put('/update/:id', checkFields, addressHandler.editAddress)
 router.delete('/delete/:id', addressHandler.deleteAddress)
 
 module.exports = router
